5f4b790731c17d717dfc818a8f30b99b56c89f68
Jonas Bernoulli <jonas@bernoul.li>
2012-05-05 02:24:56 +0200

    Revert "Fixed background color for magit diff highlighting."
    This reverts commit 2a985ac294db19327e94643eb43a38dd6289ab4b.
    
    `magit-item-highlight' (inherits from `highlight') has to be different
    from `region' because the region is relevant in the Magit status buffer.
    (The region can be staged etc. instead of the complete hunk.)

--- zenburn-theme.el
+++ zenburn-theme.el
@@ -347,7 +347,6 @@
    ;; magit
    `(magit-section-title ((,class (:foreground ,zenburn-yellow :weight bold))))
    `(magit-branch ((,class (:foreground ,zenburn-orange :weight bold))))
-   `(magit-item-highlight ((,class (:background ,zenburn-bg-1))))
 
    ;; message-mode
    `(message-cited-text ((,class (:inherit font-lock-comment))))
