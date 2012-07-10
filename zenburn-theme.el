79f6ff6fdd370d36bdbd029f795fc916fa09ffef
Bozhidar Batsov <bozhidar@tradeo.com>
2012-07-10 13:34:56 +0300

    fixed #66 - start rainbow-mode only if present

--- zenburn-theme.el
+++ zenburn-theme.el
@@ -589,7 +589,7 @@
 ;; Local Variables:
 ;; no-byte-compile: t
 ;; indent-tabs-mode: nil
-;; eval: (rainbow-mode +1)
+;; eval: (when (fboundp 'rainbow-mode) (rainbow-mode +1))
 ;; End:
 
 ;;; zenburn-theme.el ends here.
