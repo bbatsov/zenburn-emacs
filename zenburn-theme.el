151b53bca31cd1d3340e56824ec4be3b29d988f6
Bozhidar Batsov <bozhidar@tradeo.com>
2013-04-08 15:55:08 +0300

    Theme dired+

--- zenburn-theme.el
+++ zenburn-theme.el
@@ -233,6 +233,32 @@
      ((t (:background ,zenburn-bg+2 :foreground ,zenburn-fg :bold t))
       (t (:background ,zenburn-fg :foreground ,zenburn-bg :bold t))))
 
+   ;; dired+
+   `(diredp-display-msg ((t (:foreground ,zenburn-blue))))
+   `(diredp-compressed-file-suffix ((t (:foreground ,zenburn-))))
+   `(diredp-date-time ((t (:foreground ,zenburn-magenta))))
+   `(diredp-deletion ((t (:foreground ,zenburn-yellow))))
+   `(diredp-deletion-file-name ((t (:foreground ,zenburn-red))))
+   `(diredp-dir-heading ((t (:foreground ,zenburn-blue :background ,zenburn-bg-1))))
+   `(diredp-dir-priv ((t (:foreground ,zenburn-cyan))))
+   `(diredp-exec-priv ((t (:foreground ,zenburn-red))))
+   `(diredp-executable-tag ((t (:foreground ,zenburn-green+1))))
+   `(diredp-file-name ((t (:foreground ,zenburn-blue))))
+   `(diredp-file-suffix ((t (:foreground ,zenburn-green))))
+   `(diredp-flag-mark ((t (:foreground ,zenburn-yellow))))
+   `(diredp-flag-mark-line ((t (:foreground ,zenburn-orange))))
+   `(diredp-ignored-file-name ((t (:foreground ,zenburn-red))))
+   `(diredp-link-priv ((t (:foreground ,zenburn-yellow))))
+   `(diredp-mode-line-flagged ((t (:foreground ,zenburn-yellow))))
+   `(diredp-mode-line-marked ((t (:foreground ,zenburn-orange))))
+   `(diredp-no-priv ((t (:foreground ,zenburn-fg))))
+   `(diredp-number ((t (:foreground ,zenburn-green+1))))
+   `(diredp-other-priv ((t (:foreground ,zenburn-yellow+1))))
+   `(diredp-rare-priv ((t (:foreground ,zenburn-red-1))))
+   `(diredp-read-priv ((t (:foreground ,zenburn-green-2))))
+   `(diredp-symlink ((t (:foreground ,zenburn-yellow))))
+   `(diredp-write-priv ((t (:foreground ,zenburn-magenta))))
+
    ;; ert
    `(ert-test-result-expected ((t (:foreground ,zenburn-green+4 :background ,zenburn-bg))))
    `(ert-test-result-unexpected ((t (:foreground ,zenburn-red :background ,zenburn-bg))))
@@ -377,6 +403,7 @@
    `(helm-selection-line ((t (:background ,zenburn-bg+1))))
    `(helm-visible-mark ((t (:foreground ,zenburn-bg :background ,zenburn-yellow-2))))
    `(helm-candidate-number ((t (:foreground ,zenburn-green+4 :background ,zenburn-bg-1))))
+   `(helm-ff-directory ((t (:foreground ,zenburn-magenta))))
 
    ;; hl-line-mode
    `(hl-line-face ((t (:background ,zenburn-bg-05))
