868017167c06173b9664b546167f9266d2eaf1e1
mpm <mpmiszczyk@gmail.com>
2013-12-31 22:15:11 +0100

    Properties should have same colour like comments.
    And what is most important, they should be easyli readable.

--- zenburn-theme.el
+++ zenburn-theme.el
@@ -673,7 +673,7 @@ Also bind `class' to ((class color) (min-colors 89))."
    `(org-scheduled-previously ((t (:foreground ,zenburn-red))))
    `(org-scheduled-today ((t (:foreground ,zenburn-blue+1))))
    `(org-sexp-date ((t (:foreground ,zenburn-blue+1 :underline t))))
-   `(org-special-keyword ((t (:foreground ,zenburn-fg-1 :weight normal))))
+   `(org-special-keyword ((t (:inherit font-lock-comment-face))))
    `(org-table ((t (:foreground ,zenburn-green+2))))
    `(org-tag ((t (:bold t :weight bold))))
    `(org-time-grid ((t (:foreground ,zenburn-orange))))
