da41b2a9c6526d41ade60efb4718539260258c64
Bozhidar Batsov <bozhidar@tradeo.com>
2014-11-12 19:23:53 +0200

    Start work on 2.3

--- zenburn-theme.el
+++ zenburn-theme.el
@@ -4,7 +4,7 @@
 
 ;; Author: Bozhidar Batsov <bozhidar@batsov.com>
 ;; URL: http://github.com/bbatsov/zenburn-emacs
-;; Version: 2.2
+;; Version: 2.3-cvs
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
