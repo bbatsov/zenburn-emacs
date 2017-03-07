a71960eb6981797510a0ea366ea3de03ebfb685a
Bozhidar Batsov <bozhidar@batsov.com>
2017-03-07 08:48:39 +0200

    Improve some wording

--- zenburn-theme.el
+++ zenburn-theme.el
@@ -21,7 +21,7 @@
 
 ;;; Commentary:
 
-;; A port of the popular Vim theme Zenburn for Emacs 24, built on top
+;; A port of the popular Vim theme Zenburn for Emacs 24+, built on top
 ;; of the new built-in theme support in Emacs 24.
 
 ;;; Credits:
