89ecaa35dd16572ebf6715d6c120ab445caf1652
Davor Rotim <rotim.davor@gmail.com>
2017-04-28 12:31:58 +0200

    Fix background in Gnus (#271)

--- zenburn-theme.el
+++ zenburn-theme.el
@@ -617,6 +617,7 @@ Also bind `class' to ((class color) (min-colors 89))."
    `(gnus-group-news-low-empty ((t (:foreground ,zenburn-bg+2))))
    `(gnus-signature ((t (:foreground ,zenburn-yellow))))
    `(gnus-x ((t (:background ,zenburn-fg :foreground ,zenburn-bg))))
+   `(mm-uu-extract ((t (:background ,zenburn-bg-05 :foreground ,zenburn-green+1))))
 ;;;;; guide-key
    `(guide-key/highlight-command-face ((t (:foreground ,zenburn-blue))))
    `(guide-key/key-face ((t (:foreground ,zenburn-green))))
